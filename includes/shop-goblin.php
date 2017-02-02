1d5a52ed53326ffe75b031fc4840ba7ed4ab86c7
unknown <anzarsh@mail.ru>
2017-02-02 11:31:00 +0300

    add 1

---
+++ includes/shop-goblin.php
@@ -0,0 +1,9 @@
+<section class="secton_padding">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1 class="h1">Магазинчик Гоблина</h1>
+			</div>
+		</div>
+	</div>
+</section>
