34cd2d20cc0b58dd2eeefd5edbebae132ae435c4
engine77 <zaur.ka.00@mail.ru>
2017-02-02 13:15:32 +0300

    2

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 ﻿<?php require_once('includes/header.php'); ?>
-
+<?php require_once('includes/fishing.php'); ?>	
 <?php require_once('includes/protect.php'); ?>	
 <?php require_once('includes/shop-goblin.php'); ?>	
 <?php require_once('includes/footer.php'); ?>
