dcf6f6f3be4a334ba2b38c84ce64d0c1fa20b890
Berzeg07 <berezg86@gmail.com>
2017-02-02 11:32:19 +0300

    29.14

--- tech.php
+++ tech.php
@@ -1,19 +1,24 @@
 ﻿<?php require_once('includes/header.php'); ?>
+<div class="container-tech-php">
+	<br><br>
+	<h3>Кнопка</h3>	<br>		
+	<a href="#" class="button-1">В корзину</a>	
+	<br><br>
+	<div class="dd-wrap-tech-h1">
+		<h1>Пример заголовка H1</h1>
+	</div>
+	<br>
 
-<br><br>		
-<a href="#" class="button-1">Заказать звонок</a>	
-<br><br>	
-<h1>Пример заголовка</h1>
-<br><br>	
-<form action="#">
-	<input type="text" value="" placeholder="Инпут тайп текст">		
-	<input type="submit" value="Значение" placeholder="Инпут тайп сабмит">		
-</form>		
-
+	<h3>Инпут и сабмит для поиска по разделам с классами input-view, submit-view</h3>	<br>
+	<form action="#">
+		<input class="input-view" type="text" value="" placeholder="Поиск лучших товаров..">		
+		<input class="submit-view" type="submit" value="Поиск">		
+	</form>		
+</div>
 
 <?php require_once('includes/footer.php'); ?>
 
-	
+
 </body>
 </html>
 
