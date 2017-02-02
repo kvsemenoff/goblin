7784fa4455c876bf0a5ce920ff155a541b69ad28
engine77 <zaur.ka.00@mail.ru>
2017-02-02 14:23:12 +0300

    2

---
+++ includes/fishing.php
@@ -0,0 +1,17 @@
+
+<section class="section_padding df-section-bg">
+	<div class="df-section">	
+		<div class="df-fish df-fish_view">
+			<div class="df-fish__R df-fish__R_view">
+				<iframe width="100%" height="100%" src="https://www.youtube.com/embed/lVIdC4b2rtk" frameborder="0" allowfullscreen></iframe>
+			</div>
+			<div class="df-fish__L df-fish__L_view">
+				<h1>Беспроводной эхолот для рыбалки</h1>
+				<span>Незаменимый помощник на рыбалке. Многофункционален, компактен и прост в эксплуатации.</span>
+				<a href="#" class="df-bynow_view">купить сейчас</a>
+				<a href="#" class="df-readmore_view">подробне</a>
+			</div>			
+		</div>
+		<div class="clear"></div>
+	</div>	
+</section>				
