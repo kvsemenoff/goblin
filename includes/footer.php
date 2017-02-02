532e7979c6527d98b03614d84fc0994fa88e5942
ibragimtokmak <tokmakovibragim@gmail.com>
2017-02-02 11:54:35 +0300

    if

---
+++ includes/footer.php
@@ -0,0 +1,28 @@
+<section class="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+               <div class="footer__left">
+                   <div class="footer__logo-block">
+                       <div class="footer__logo-img">
+                           <img src="/img/logo.png" alt="Магазинчик Гоблина">
+                       </div>
+                       <div class="footer__info">
+                           <p class="footer__p webaddr"><span class="opershop">OperShop</span>.ru</p>
+                           <p class="footer__p slogan">Купить из тупичка</p>
+                       </div>       
+                   </div>
+                   <div class="footer__cooperate-block">
+                       <p class="footer__p cooperate">По поводу сотрудничества пишите: <span style="color: #a2a29c">shop@oper.ru</span> </p>
+                   </div>
+               </div>
+               <div class="footer__right">
+                   <div class="footer__actions"><p class="footer__p actions__boldtext">Узнать первым</p><p class="footer__p actions__text">об акциях, интересных поступлениях</p></div>
+                   <div class="footer__email"></div>
+                   <p class="footer__p copyright">&copy; 2016 Goblin EnterTorMent. Все права защищены.</p>
+                   <a href="" class="footer__scroll-top"></a>
+               </div>
+            </div>
+        </div>
+    </div>
+</section>            
