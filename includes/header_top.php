9aadf8216fd09b6d17e3c886c178e8bdf1a45cc5
Berzeg07 <berezg86@gmail.com>
2017-02-03 17:21:33 +0300

    29.14

--- includes/header_top.php
+++ includes/header_top.php
@@ -1,7 +1,234 @@
-<section>
+<section class="header-wrap">
 	<div class="container">
 		<div class="row">
-			ddd
+			<div class="dd-logo">
+				<a href="#"><img class="img-responsive" src="img/logo.png" alt=""></a>
+			</div>
+			<div class="dd-korzina">
+				<div class="dd-reg-wrap">
+					<span><a href="#">Регистрация</a></span>
+					<span><a href="#">Войти</a></span>
+				</div>
+				<a href="#">
+					<div class="dd-korzina-img">
+						<img src="img/korz.png" alt="">
+					</div>
+				</a>
+				<div class="dd-korzina-txt">
+					<span>Корзина</span><br>
+					<span>добавьте товары</span>
+				</div>
+				<div class="dd-burger">
+					<img src="img/burg.png" alt="">
+				</div>
+			</div>
+			<div class="clearfix clearfix-display"></div>
+			<label class="dd-input-header">
+				<input type="text" class="input-view input-view-header" placeholder="Поиск лучших товаров..">
+				<input class="lupa-submit"	type="submit" value="">
+			</label>
+			<div class="clearfix clearfix-display2"></div>
 		</div>
 	</div>
 </section>
+
+<!-- Меню для дектопа -->
+<section class="dd-menu-dekstop-wrap">
+	<div class="container">
+		<div class="row">
+			<div class="dd-menu">
+				<ul>
+					<li>
+						<a href="#">
+							<span>Каталог</span><br>
+							<span>Все категории</span>
+						</a>
+					</li>
+
+					<li>
+						<a href="#">
+							<span>Электорника</span><br>
+							<span>и компьютеры</span>
+						</a>
+					</li>
+
+					<li>
+						<a href="#">
+							<span>Спорт</span><br>
+							<span>и отдых</span>
+						</a>
+					</li>
+
+					<li>
+						<a href="#">
+							<span>Игрушки</span><br>
+							<span>для детей</span>
+						</a>
+					</li>
+
+					<li>
+						<a href="#">
+							<span>авто</span><br>
+							<span>товары</span>
+						</a>
+					</li>
+
+					<li>
+						<a href="#">
+							<span>дом, сад, огород</span><br>
+							<span>инструменты</span>
+						</a>
+					</li>
+
+					<li>
+						<a href="#">
+							<span>игры, музыка</span><br>
+							<span>и видеоозвучки</span>
+						</a>
+					</li>
+				</ul>
+			</div>	
+		</div>
+	</div>
+</section>
+<!-- Конец меню для декстопа -->
+
+<!-- Submenu -->
+<!-- <div class="container">
+	<div class="row"> -->
+		<div class="dd-sub-menu">
+			<div class="dd-reg-mobile">
+				<a href="#">Регистрация</a>
+				<a href="#">Войти</a>
+			</div>
+			<div class="dd-left">
+				<div class="dd-left-center">
+					<div class="dd-box dd-box1">
+						<ul>
+							<li>
+								<a href="#">
+									<span>Гоблин рекомендует</span><br>
+									<span>136 товаров</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span>Товары с подарками</span><br>
+									<span>27 товаров</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="dd-box dd-box2">
+						<ul>
+							<li>
+								<a href="#">
+									<span>Хиты октября</span><br>
+									<span>14 товаров</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span>Сделан видеоразбор</span><br>
+									<span>249 товаров</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="clearfix sub-clearfix"></div>
+				</div>
+			</div>
+			<div class="dd-left-center">
+				<div class="dd-center">
+					<div class="dd-box-center">
+						<div class="dd-title-dekstop">
+							<span>Каталог товаров</span>
+						</div>	
+						<ul>
+							<li>
+								<a href="#">
+									Аксессуары для ноутбуков
+									и ПК
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									Авто-товары
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									Игры, музыка
+									и видеоозвучки
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									Электроника и компьютеры
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									Спорт и отдых
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									Игрушки для детей
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									Красота и здоровье
+								</a>
+							</li>
+						</ul>
+					</div>	
+				</div>	
+				<div class="dd-center dd-right">
+					<div class="dd-box-center">
+						<ul>
+							<li>
+								<a href="#">
+									Одежда, обувь аксессуары
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									Зоотовары
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									Канцелярские товары
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									Бытовая техника
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									Книги
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									Антиквариат, винтаж, искусство
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									Товары для взрослых
+								</a>
+							</li>
+						</ul>
+					</div>	
+				</div>
+			</div>	
+			<div class="clearfix"></div>	
+		</div>
+<!-- 	</div>
+</div> -->
+<!-- Конец submenu -->
