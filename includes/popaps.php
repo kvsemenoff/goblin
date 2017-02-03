efee9c034ed89902d7d586896a54dcc311e87788
unknown <anzarsh@mail.ru>
2017-02-03 13:15:16 +0300

    add 1

---
+++ includes/popaps.php
@@ -0,0 +1,39 @@
+<div id="mask"></div>
+<div class="db-popaps-form1">
+	<h1 class="db-popaps-h1">Заказать обратный звонок</h1>
+	<form action="#" method="post" class="db-form-1">
+		<input type="text" name="uname" class="input-view input-view_width input-view input-view_db-popaps1" placeholder="Введите ваше имя"><br>
+		<input type="text" name="email" class="input-view input-view_width input-view_db-popaps1" placeholder="Укажите вашу почту"><br>
+		<textarea name="comment" class="textarea-view_db-popaps1" id="" cols="33" rows="8" placeholder="Ваш комментарий"></textarea><br>
+		<input type="submit" name="submit" class="submit-view submit-view-width db-submit-popaps" value="Отправить сейчас">
+	</form>
+	<span class="db-close1"></span>
+</div>
+<div class="db-popaps-form2">
+	<h1 class="db-popaps-h1">Заказать обратный звонок</h1>
+	<form action="#" method="post" class="db-form-1">
+		<input type="text" name="uname" class="input-view input-view_width input-view input-view_db-popaps2" placeholder="Введите ваше имя"><br>
+		<input type="text" name="email" class="input-view input-view_width input-view_db-popaps2" placeholder="Укажите вашу почту"><br>
+		<textarea name="comment" class="textarea-view_db-popaps2" id="" cols="33" rows="8" placeholder="Ваш комментарий"></textarea><br>
+		<input type="submit" name="submit" class="submit-view submit-view-width db-submit-popaps" value="Отправить сейчас">
+	</form>
+	<span class="db-close1"></span>
+</div>
+<div class="db-popaps-form3">
+	<h1 class="db-popaps2-h1">Заказать обратный звонок</h1>
+	<form action="#" method="post" class="db-form-1">
+		<input type="text" name="uname" class="input-view input-view_width input-view input-view_db-popaps3" placeholder="Введите ваше имя"><br>
+		<input type="text" name="email" class="input-view input-view_width input-view_db-popaps3" placeholder="Укажите вашу почту"><br>
+		<textarea name="comment" class="textarea-view_db-popaps3" id="" cols="33" rows="8" placeholder="Ваш комментарий"></textarea><br>
+		<input type="submit" name="submit" class="submit-view submit-view-width db-submit-popaps" value="Отправить сейчас">
+	</form>
+	<span class="db-close2"></span>
+</div>
+<div class="db-popaps-form4">
+	<img src="img/db-message.png" alt="img"><br>
+	<h1 class="db-message-h1">Ваша заявка<br>
+		успешно отправлена</h1>
+		<p class="db-message-p">наш представитель свяжется с вами в<br> ближайшее время</p>
+		<a href="#" class="db-message-a">ЗАКРЫТЬ</a>
+		<span class="db-close2"></span>
+	</div>
