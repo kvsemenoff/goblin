4608ace0bcd19ef7e22e8070dc99591a35d58f41
Berzeg07 <berezg86@gmail.com>
2017-02-03 09:45:12 +0300

    29.14

--- js/common.js
+++ js/common.js
@@ -34,6 +34,27 @@ $(document).ready(function(){
         $(this).toggleClass('az-select-focus');
     });
 
- });
+
+    $(".df-number .dfbutton").on("click", function() {
+
+        var $button = $(this);
+        var oldValue = $button.parent().find("input").val();  
+
+        if ($button.text() == "+") {
+          var newVal = parseFloat(oldValue) + 1;
+      }  
+      else {
+     // Don't allow decrementing below zero
+     if (oldValue > 1) {
+        var newVal = parseFloat(oldValue) - 1;
+    } else {
+        newVal = 1;
+    }
+}
+
+$button.parent().find("input").val(newVal);
+
+});
+});
 
 
