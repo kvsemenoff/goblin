79df1200ebd428b2fbfd6936e4da5094d81d28d1
Berzeg07 <berezg86@gmail.com>
2017-02-06 17:15:28 +0300

    29.14

--- catalog.php
+++ catalog.php
@@ -30,30 +30,47 @@
 				
 			</div>
 			<div class="col-md-9 padding-none">
-				<div class="dd-sortirovka float-left-only">
-					<span>Сортировка</span>
+				<div class="dd-cat">
+					<div class="dd-sortirovka float-left-only">
+						<span>Сортировка</span>
+					</div>
+					<div class="az-select az-select-reiting float-left-only">
+						<select name="" id="">
+							<option value="1">По рейтингу</option>
+							<option value="2">тест</option>
+							<option value="3">тест</option>
+							<option value="4">тест</option>
+							<option value="5">тест</option>
+							<option value="6">тест</option>
+							<option value="7">тест</option>
+							<option value="8">тест</option>
+							<option value="9">тест</option>
+							<option value="9">тест</option>
+						</select>
+					</div>
+					<div class="dd-sortirovka float-right-only">
+						<span>Показать только:</span>
+						<a class="dd-a-recomend" href="#">гоблин рекомендует</a>
+						<a class="dd-a-recomend" href="#">с подарками</a>
+						<a class="dd-a-recomend2"href="#">популярные за октябрь</a>
+					</div>
+					<div class="clearfix"></div>
 				</div>
-				<div class="az-select az-select-reiting float-left-only">
-					<select name="" id="">
-						<option value="1">По рейтингу</option>
-						<option value="2">тест</option>
-						<option value="3">тест</option>
-						<option value="4">тест</option>
-						<option value="5">тест</option>
-						<option value="6">тест</option>
-						<option value="7">тест</option>
-						<option value="8">тест</option>
-						<option value="9">тест</option>
-						<option value="9">тест</option>
-					</select>
+				<?php require_once('includes/catalogproduct.php'); ?>	
+				<div class="section_padding ">
+					<div class="df-pagination df-pagination_view">
+						<a href="#" class="df-pagination__link1 df-pagination__link1_view">Назад</a>
+						<div class="df-pagination__numbers df-pagination__numbers_view">
+							<a href="#">1</a>
+							<a href="#">2</a>
+							<a href="#">3</a>
+							<a href="#">4</a>
+							<a href="#">5</a>
+							<a href="#">6</a>	
+						</div>
+						<a href="#" class="df-pagination__link2 df-pagination__link2_view">вперед</a>
+					</div>
 				</div>
-				<div class="dd-sortirovka float-right-only">
-					<span>Показать только:</span>
-					<a class="dd-a-recomend" href="#">гоблин рекомендует</a>
-					<a class="dd-a-recomend" href="#">с подарками</a>
-					<a class="dd-a-recomend2"href="#">популярные за октябрь</a>
-				</div>
-				<div class="clearfix"></div>
 			</div>	
 		</div>
 	</div>
