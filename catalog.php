48fc9bcd8cf10ac9e5ca9f5a34d463f9b8d5cfd0
Berzeg07 <berezg86@gmail.com>
2017-02-06 15:03:36 +0300

    29.14

---
+++ catalog.php
@@ -0,0 +1,42 @@
+﻿<?php require_once('includes/header.php'); ?>
+<?php require_once('includes/header_top.php'); ?>
+
+
+
+
+<section class="dd-brad-wrap">
+	<div class="container">
+		<div class="row">
+			<div class="dd-bradcrumps">
+				<a href="#">Главная</a><span>Каталог</span>
+			</div>	
+			<div class="dd-catalog-title">
+				<span>Товары из тупичка</span>
+				<span>Подобрано 326 товаров</span>
+			</div>
+			<div class="dd-knopki">
+				<a href="#"></a>
+				<a href="#"></a>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+</section>
+
+
+
+
+
+
+
+
+<?php require_once('includes/protect.php'); ?>	
+<?php require_once('includes/shop-goblin.php'); ?>	
+<?php require_once('includes/footer.php'); ?>
+<?php require_once('includes/popaps.php'); ?>
+
+
+
+</body>
+</html>
+
