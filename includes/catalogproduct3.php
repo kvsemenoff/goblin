c06e3f7552f81ba74aa3227fd00fe51f2d4b3f98
Berzeg07 <berezg86@gmail.com>
2017-02-07 12:22:45 +0300

    29.14

--- includes/catalogproduct3.php
+++ includes/catalogproduct3.php
@@ -1,27 +1,27 @@
 <section class="catalog-tablet-wrap">
 	<div class="dd-left-cat">
-			<div class="ch-block">
-									<label class="radio-lable">
-										<input class="checkbox" type="radio" name="checkbox-test" checked>
-										<span class="checkbox-custom checkbox-custom--grey"></span>
-										<span class="label"></span>
-									</label>
-									<label class="radio-lable">
-										<input class="checkbox" type="radio" name="checkbox-test" checked>
-										<span class="checkbox-custom checkbox-custom--black"></span>
-										<span class="label"></span>
-									</label>
-									<label class="radio-lable">
-										<input class="checkbox" type="radio" name="checkbox-test" checked>
-										<span class="checkbox-custom checkbox-custom--green"></span>
-										<span class="label"></span>
-									</label>
-									<label class="radio-lable">
-										<input class="checkbox" type="radio" name="checkbox-test" checked>
-										<span class="checkbox-custom checkbox-custom--light"></span>
-										<span class="label"></span>
-									</label>
-								</div>
+		<div class="ch-block">
+         <label class="radio-lable">
+          <input class="checkbox" type="radio" name="checkbox-test" checked>
+          <span class="checkbox-custom checkbox-custom--grey"></span>
+          <span class="label"></span>
+         </label>
+         <label class="radio-lable">
+          <input class="checkbox" type="radio" name="checkbox-test" checked>
+          <span class="checkbox-custom checkbox-custom--black"></span>
+          <span class="label"></span>
+         </label>
+         <label class="radio-lable">
+          <input class="checkbox" type="radio" name="checkbox-test" checked>
+          <span class="checkbox-custom checkbox-custom--green"></span>
+          <span class="label"></span>
+         </label>
+         <label class="radio-lable">
+          <input class="checkbox" type="radio" name="checkbox-test" checked>
+          <span class="checkbox-custom checkbox-custom--light"></span>
+          <span class="label"></span>
+         </label>
+        </div>
 	</div>
 	<div class="dd-center-cat">
 		
