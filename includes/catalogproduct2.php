a292dc5623d24726aba1d728d8e07d467f72698b
Berzeg07 <berezg86@gmail.com>
2017-02-07 10:12:17 +0300

    29.14

--- includes/catalogproduct2.php
+++ includes/catalogproduct2.php
@@ -19,6 +19,7 @@
 				</div>
 			</div>
 		</div>
+		<!-- Конец Toshiba -->
 
 		<div class="df-catalog__product df-catalog__product_view">
 			<div class="df-catalog__img df-catalog__img_view">
@@ -65,7 +66,7 @@
 			</div>
 		</div>
 		
-
+		<!-- Игра -->
 		<div class="df-catalog__product df-catalog__product_view">
 			<div class="df-catalog__img df-catalog__img_view">
 				<img src="img/pole.jpg" alt="" class="df-imgproduct">
@@ -111,9 +112,10 @@
 				</div>
 			</div>
 		</div>
+		<!-- Конец игра -->
 		
 		<!-- Наушники -->
-		
+
 		<div class="df-catalog__product df-catalog__product_view">
 			<div class="df-catalog__img df-catalog__img_view">
 				<img src="img/dftovar6.png" alt="" class="df-imgproduct">
@@ -139,6 +141,153 @@
 				</div>
 			</div>
 		</div>
+
+		<!-- Конец наушники -->
+
+		<!-- Комп -->
+		<div class="df-catalog__product df-catalog__product_view">
+			<div class="df-catalog__img df-catalog__img_view">
+				<img src="img/dftovar7.png" alt="" class="df-imgproduct">
+				<div class="df-miniimage margin-top-minus">
+					<div class="df-catalog__ico df-catalog__ico_view">
+						<span class="df-catalog__show df-catalog__show_view">+ подарок</span>
+						<div class="df-catalog_image df-catalog_image_view">
+							<img src="img/dfi1.png" alt="">
+						</div>
+					</div>
+					<div class="df-catalog__ico df-catalog__ico_view">
+						<span class="df-catalog__show df-catalog__show_view">сделан видеоразбор</span>
+						<div class="df-catalog_image df-catalog_image_view">
+							<img src="img/dfi2.png" alt="">
+						</div>
+					</div>
+					<div class="df-catalog__ico df-catalog__ico_view " id="df-another">
+						<span class="df-catalog__show df-catalog__show_view">гоблин рекомендует</span>
+						<div class="df-catalog_image df-catalog_image_view">
+							<img src="img/dfi3.png" alt="">
+						</div>
+					</div>
+					<div class="df-catalog__ico df-catalog__ico_view">
+						<span class="df-catalog__show df-catalog__show_view">хит октября</span>
+						<div class="df-catalog_image df-catalog_image_view">
+							<img src="img/dfi4.png" alt="">
+						</div>
+					</div>
+				</div>			
+			</div>
+			<div class="df-catalog-txtbox df-catalog-txtbox_view">
+				<div class="df-alltxt">
+					<h4>Игровой <br>компьютер <br>REVOLT X</h4>
+					<span class="df-nameproduct">Игровые компьютеры</span>	
+				</div>
+				<div class="dfboxprice">			
+					<div class="df-catalog__busket df-catalog__busket_view df-block">
+						от 400 000 р
+					</div>
+					<a href="#" class="button-1 button-1_view">в корзину</a>
+				</div>
+			</div>
+		</div>
+		<!-- Конец комп -->
+
+		<!-- Fostex -->
+
+		<div class="df-catalog__product df-catalog__product_view">
+			<div class="df-catalog__img df-catalog__img_view">
+				<img src="img/dftovar8.png" alt="" class="df-imgproduct">
+				<div class="df-miniimage">					
+					<div class="df-catalog__ico df-catalog__ico_view">
+						<span class="df-catalog__show df-catalog__show_view">сделан видеоразбор</span>
+						<div class="df-catalog_image df-catalog_image_view">
+							<img src="img/dfi2.png" alt="">
+						</div>
+					</div>					
+				</div>
+			</div>
+			<div class="df-catalog-txtbox df-catalog-txtbox_view">
+				<div class="df-alltxt df-alltxt3">
+					<h4>Fostex HP-A4BL</h4>
+					<span class="df-nameproduct">Усилители</span>
+					<p>это цифро-аналоговый преобразователь и усилитель для наушников, который представляет собой усовершенствованную версию модели HP-A4. Главное отличие новинки: наличие..</p>
+				</div>
+				<div class="dfboxprice">
+					<div class="df-catalog__busket df-catalog__busket_view">
+						<span class="df-catalog__oldprice df-catalog__oldprice_view">35 990 р</span>
+						<span class="df-catalog__newprice df-catalog__newprice_view">26 690 р</span>
+					</div>
+					<a href="#" class="button-1 button-1_view">в корзину</a>
+				</div>
+			</div>
+		</div>
+		<!-- Конец Fostex -->
+
+
+		<div class="df-catalog__product df-catalog__product_view">
+			<div class="df-catalog__img df-catalog__img_view">
+				<img src="img/dftovar2.png" alt="" class="df-imgproduct">
+				<div class="df-miniimage margin-top-minus">
+					<div class="df-catalog__ico df-catalog__ico_view">
+						<span class="df-catalog__show df-catalog__show_view">+ подарок</span>
+						<div class="df-catalog_image df-catalog_image_view">
+							<img src="img/dfi1.png" alt="">
+						</div>
+					</div>
+					<div class="df-catalog__ico df-catalog__ico_view">
+						<span class="df-catalog__show df-catalog__show_view">сделан видеоразбор</span>
+						<div class="df-catalog_image df-catalog_image_view">
+							<img src="img/dfi2.png" alt="">
+						</div>
+					</div>
+					<div class="df-catalog__ico df-catalog__ico_view " id="df-another">
+						<span class="df-catalog__show df-catalog__show_view">гоблин рекомендует</span>
+						<div class="df-catalog_image df-catalog_image_view">
+							<img src="img/dfi3.png" alt="">
+						</div>
+					</div>
+					<div class="df-catalog__ico df-catalog__ico_view">
+						<span class="df-catalog__show df-catalog__show_view">хит октября</span>
+						<div class="df-catalog_image df-catalog_image_view">
+							<img src="img/dfi4.png" alt="">
+						</div>
+					</div>
+				</div>							
+			</div>
+			<div class="df-catalog-txtbox df-catalog-txtbox_view">
+				<div class="df-alltxt">
+					<h4>Toshiba <br> HDTS425EZSTA</h4>
+					<span class="df-nameproduct">Компьютерные комплектующие</span>
+					<p>Это семейство твердотельных накопителей (SSD), создано с использованием флэш-памяти NAND, разработанной корпорацией Toshiba на базе технологии TLC TLC A19nm NAND Flash</p>
+				</div>
+				<div class="dfboxprice">
+					<div class="df-catalog__busket df-catalog__busket_view">
+						<span class="df-catalog__newprice2 df-catalog__newprice_view2">26 190 р</span>
+					</div>
+					<a href="#" class="button-1 button-1_view">в корзину</a>
+				</div>
+			</div>
+		</div>
+
+		<!-- Конец Toshiba2 -->
+		<!-- Мфу продан -->
+		<div class="df-catalog__product df-catalog__product_view">
+			<div class="df-catalog__img df-catalog__img_view">
+				<img src="img/dftovar3.png" alt="" class="df-imgproduct">
+			</div>
+			<div class="df-catalog-txtbox df-catalog-txtbox_view">
+				<div class="df-alltxt">
+					<h4>МФУ Ricoh <br> SP 150SUW</h4>
+					<span class="df-nameproduct">Оперативная память</span>
+					<p>Элегантный дизайн, интуитивно понятное управление и скорость печати 22 стр./мин. Простота установки драйвера с автозапуском виртуальной панели управления позволяет начать работу..</p>
+				</div>
+				<div class="dfboxprice">
+					<div class="df-catalog__busket df-catalog__busket_view">
+						<span class="df-catalog__newprice2 df-catalog__newprice_view2">8 210 р</span>
+					</div>
+					<a href="#" class="button-1 button-1_view">в корзину</a>
+				</div>
+			</div>
+		</div>
+		<!-- Конец мфу продан -->
 
 	<!-- 	<div class="df-catalog__product df-catalog__product_view">
 			<div class="df-catalog__img df-catalog__img_view">
@@ -242,51 +391,8 @@
 		</div>
 
 
-		<div class="df-catalog__product df-catalog__product_view">
-			<div class="df-catalog__img df-catalog__img_view">
-				<img src="img/dftovar7.png" alt="" class="df-imgproduct">			
-			</div>
-			<div class="df-catalog-txtbox df-catalog-txtbox_view">
-				<div class="df-alltxt">
-					<h4>Игровой <br>компьютер <br>REVOLT X</h4>
-					<span class="df-nameproduct">Игровые компьютеры</span>	
-				</div>
-				<div class="dfboxprice">			
-					<div class="df-catalog__busket df-catalog__busket_view df-block">
-						от 400 000 р
-					</div>
-					<a href="#" class="button-1 button-1_view">в корзину</a>
-				</div>
-			</div>
-		</div>
 
-		<div class="df-catalog__product df-catalog__product_view">
-			<div class="df-catalog__img df-catalog__img_view">
-				<img src="img/dftovar8.png" alt="" class="df-imgproduct">
-				<div class="df-miniimage">					
-					<div class="df-catalog__ico df-catalog__ico_view">
-						<span class="df-catalog__show df-catalog__show_view">сделан видеоразбор</span>
-						<div class="df-catalog_image df-catalog_image_view">
-							<img src="img/dfi2.png" alt="">
-						</div>
-					</div>					
-				</div>
-			</div>
-			<div class="df-catalog-txtbox df-catalog-txtbox_view">
-				<div class="df-alltxt df-alltxt3">
-					<h4>Fostex HP-A4BL</h4>
-					<span class="df-nameproduct">Усилители</span>
-					<p>это цифро-аналоговый преобразователь и усилитель для наушников, который представляет собой усовершенствованную версию модели HP-A4. Главное отличие новинки: наличие..</p>
-				</div>
-				<div class="dfboxprice">
-					<div class="df-catalog__busket df-catalog__busket_view">
-						<span class="df-catalog__oldprice df-catalog__oldprice_view">35 990 р</span>
-						<span class="df-catalog__newprice df-catalog__newprice_view">26 690 р</span>
-					</div>
-					<a href="#" class="button-1 button-1_view">в корзину</a>
-				</div>
-			</div>
-		</div>
+
 		<a href="#">
 			<div class="df-catalog__product df-catalog__product_view">
 				<div class="dd-krug">
