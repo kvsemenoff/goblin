6c1212407386269aa77b3ae5f40a823a9f6b5582
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-02-10 10:59:10 +0600

    complete cart.php

--- js/common.js
+++ js/common.js
@@ -164,6 +164,29 @@ $button.parent().find("input").val(newVal);
         }
     });
 
+// ==========================================
+// ========== Табы cart.php ==========
+// ==========================================
+$('.tabs-content > .tab-content').each(function(index){
+    if (index != 0) {
+        $(this).css('display','none');
+    };
+});
+    
+$('.tab').click(function(){
+    $('.tab').removeClass('active');
+    $('.tabs-content').find('div').removeClass('active');
+    $('.tabs-content > .tab-content').css('display','none');
+    $(this).addClass('active');
+
+    for (var i = 0; i < 4; i++) {
+        if($('.tabs > .tab-' + i).hasClass('active')){
+            $('.tabs-content > .tab-' + i).addClass('active');
+    };
+    
+    $('.tabs-content > .active').css('display','block');
+    };
+});
 
 
 // Главное меню
