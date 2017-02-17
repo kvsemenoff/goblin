5e99da649e495c09f3395a70ce1334a5ccc9cbf0
Berzeg07 <berezg86@gmail.com>
2017-02-17 09:48:38 +0300

    29.14

---
+++ guide.php
@@ -0,0 +1,124 @@
+<?php require_once('includes/header.php'); ?>
+
+<section class="color-pallet-wrap">
+	<div class="container">
+		<div class="cl-title">
+			Color Palette
+		</div>
+		<div class="cl-pallette-box">
+			<ul>
+				<li>
+					<div class="cl-box cl-box1"></div>
+					<span>#e3181b</span>
+					<div class="clearfix"></div>
+				</li>
+				<li>
+					<div class="cl-box cl-box2"></div>
+					<span>#ffff00</span>
+					<div class="clearfix"></div>
+				</li>
+				<li>
+					<div class="cl-box cl-box3"></div>
+					<span>#ffa500</span>
+					<div class="clearfix"></div>
+				</li>
+				<li>
+					<div class="cl-box cl-box4"></div>
+					<span>#d0d0c0</span>
+					<div class="clearfix"></div>
+				</li>
+				<li>
+					<div class="cl-box cl-box5"></div>
+					<span>#a9a99c</span>
+					<div class="clearfix"></div>
+				</li>
+				<li>
+					<div class="cl-box cl-box6"></div>
+					<span>#74746c</span>
+					<div class="clearfix"></div>
+				</li>
+			</ul>
+		</div>
+		<div class="cl-pallette-box cl-pallette-box2">
+			<ul>
+				<li>
+					<div class="cl-box cl-box7"></div>
+					<span>#fff</span>
+					<div class="clearfix"></div>
+				</li>
+				<li>
+					<div class="cl-box cl-box8"></div>
+					<span>#1e1e1e</span>
+					<div class="clearfix"></div>
+				</li>
+				<li>
+					<div class="cl-box cl-box9"></div>
+					<span>#282828</span>
+					<div class="clearfix"></div>
+				</li>
+				<li>
+					<div class="cl-box cl-box10"></div>
+					<span>#2c2c2c</span>
+					<div class="clearfix"></div>
+				</li>
+			</ul>
+		</div>
+	</div>
+</section>
+<!-- Конец Color Palette -->
+
+<!-- Tipography -->
+
+<section class="tp-wrapper">
+	<div class="container">
+		<div class="cl-title">
+			Tipography
+		</div>
+		<div class="tp-container">
+			<div class="tp-box">
+				<h1 class="db-shop-h1">Магазинчик <span>Гоблина</span></h1>
+				<div class="dd-inner-box">
+					<p class="tp-1">Header1: Ubuntu Bold 34px, spacing 40px</p>
+					<p class="dd-katalog">Каталог <span>Дом, Сад, Огород <span> Корзина</span></span></p>
+					<p class="tp-1">
+						Title menu, additional header: Ubuntu Bold 14px, spacing 22px
+					</p>
+					<p class="tp-2">
+						Незаменимый помощник на рыбалке. Многофункционален,<br>
+						компактен и прост в эксплуатации.
+					</p>
+					<p class="tp-1">
+						Tesis for header1: Roboto Regular 16px, spacing 22px
+					</p>
+				</div>
+			</div>
+			<div class="tp-box">
+				<span class="tp-title">Беспроводной эхолот для рыбалки</span>
+				<p class="tp-1">
+					Header2: Ubuntu Bold 22px, spacing 27px
+				</p>
+				<p class="tp-italic">
+					Приходите покупайте, покупайте приходите
+				</p>
+			</div>	
+			<div class="clearfix"></div>	
+		</div>
+	</div>
+</section>
+
+<!-- Конец Tipography -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
