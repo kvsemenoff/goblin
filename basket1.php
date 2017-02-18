047aa556265520c42acaae0bdcb1e3d30ee47f2c
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-02-18 09:17:00 +0600

    complete 1 block to basket

--- basket1.php
+++ basket1.php
@@ -4,141 +4,254 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-					<div class="box-1">
-						<h3 class="item__box-1">Корзина</h3>
-						<div class="clearfix">
-							<span class="box__title box__title-img">Товар</span>
-							<span class="box__title box__title-txt">Краткая информация</span>
-							<span class="box__title box__title-quan">Кол-во</span>
-							<span class="box__title box__title-price">Цена</span>
-						</div>
+				<div class="box-1">
+					<h3 class="item__box-1">Корзина</h3>
+					<div class="clearfix">
+						<span class="box__title box__title-img">Товар</span>
+						<span class="box__title box__title-txt">Краткая информация</span>
+						<span class="box__title box__title-quan">Кол-во</span>
+						<span class="box__title box__title-price">Цена</span>
 					</div>
-					<div class="box-2">
-						<div class="clearfix">
-							<div class="nam nam-img">
-								<div class="item__img">
-									<img src="img/busket_v2.jpg" alt="">
-								</div>
+				</div>
+				<div class="box-2">
+					<div class="clearfix">
+						<div class="nam nam-img">
+							<div class="item__img">
+								<img src="img/busket_v2.jpg" alt="">
 							</div>
-							<div class="nam nam-txt">
-								<h4 class="item__info">TOSHIBA HDTS425EZSTA</h4>
-								<div class="name12">Оперативная память </div>
-								<div class="name2">Белый / XL 48 /480 ГБ / 2.5" SATA III</div>
-								<div class="name2">+ 2 подарка (дополнительная щетка, набор валиков)</div>
-							</div>	
-							<div class="nam nam-quan">
-								<div class="df-number">
-									<span class="dfbutton dfbutton2">-</span>
-									<input type="text" value="1" disabled="disabled">  
-									<span class="dfbutton dfbutton3">+</span>
-								</div>	
-							</div>
-							<div class="nam nam-price">
-								<div class="item__price">
-									<strong>52 380 Р</strong>
-									<div class="name12"> 1шт.: 26 190 р.</div>	
-								</div>
-							</div>
-							<button class="nam-close__btn"></button>
 						</div>
-					</div>
-					<div class="box-2">
-						<div class="clearfix">
-							<div class="nam nam-img">
-								<div class="item__img">
-									<img src="img/busket_v3.jpg" alt="">
-								</div>
-							</div>
-							<div class="nam nam-txt">
-								<h4 class="item__info">Игровой компьютер REVOLT X</h4>
-								<div class="name12">Игровые компьютеры</div>
-								<div class="name2">Core i7 6700k / 2xGeForce GTX 1080 / 32Gb DDR4 / SSD 250 Gb / HDD 2Tb</div>
-								<div class="name2">+ Специальное предложение X3Экономия 231Р.</div>
-							</div>	
-							<div class="nam nam-quan">
-								<div class="df-number">
-									<span class="dfbutton dfbutton2">-</span>
-									<input type="text" value="1" disabled="disabled">  
-									<span class="dfbutton dfbutton3">+</span>
-								</div>	
-							</div>
-							<div class="nam nam-price">
-								<div class="item__price">
-									<strong>278 440 Р</strong>
-									<div class="name12"> 1шт.: 278 440 р.</div>	
-								</div>
-							</div>
-							<button class="nam-close__btn"></button>
-						</div>
-					</div>
-					<div class="box-2">
-						<div class="clearfix">
-							<div class="nam nam-img">
-								<div class="item__img">
-									<img src="img/busket_v4.jpg" alt="">
-								</div>
-							</div>
-							<div class="nam nam-txt">
-								<h4 class="item__info">умная метка nut</h4>
-								<div class="name12">Аксессуары к мобильным устройствам</div>
-								<div class="name2">Батарея СК2032 / Макс. дальн. 50 метров / Iphone 4S и выше / OC Android 4.3 и выше</div>
-								<div class="name2">+ Специальное предложение X10Экономия 231Р и подарок (Мягкий магнитный калькулятор на холодильник)</div>
-							</div>	
-							<div class="nam nam-quan">
-								<div class="df-number">
-									<span class="dfbutton dfbutton2">-</span>
-									<input type="text" value="1" disabled="disabled">  
-									<span class="dfbutton dfbutton3">+</span>
-								</div>	
-							</div>
-							<div class="nam nam-price">
-								<div class="item__price">
-									<strong>9 979 Р</strong>
-									<div class="name12"> 1шт.: 1 070р.</div>	
-								</div>
-							</div>
-							<button class="nam-close__btn"></button>
-						</div>
-					</div>
-					<div class="box-2">
-						<div class="clearfix">
-							<div class="nam nam-img">
-								<div class="item__img">
-									<img src="img/busket_v5.jpg" alt="">
-								</div>
-							</div>
-							<div class="nam nam-txt">
-								<h4 class="item__info">Эксклюзивная модель танка Пантера</h4>
-								<div class="name12">Игровые компьютеры</div>
-								<div class="name2">Ручная работа / Проработка / Масштаб 1к35 / Надежная упаковка</div>
-								<div class="name2"></div>
-							</div>	
-							<div class="nam nam-quan">
-								<div class="df-number">
-									<span class="dfbutton dfbutton2">-</span>
-									<input type="text" value="1" disabled="disabled">  
-									<span class="dfbutton dfbutton3">+</span>
-								</div>	
-							</div>
-							<div class="nam nam-price">
-								<div class="item__price">
-									<strong>8 200 Р</strong>
-									<div class="name12"> 1шт.: 8 200 р.</div>	
-								</div>
-							</div>
-							<button class="nam-close__btn"></button>
-						</div>
-					</div>
-					<div class="box-itog">
-						<div class="clearfix">
-							<div class="item__itog"><span>
-								<div class="itog">Итого: 4 товара на сумму <span> 350 980 Р.</span> 
-								</div>
-							</div>
-							<div class="item__itog item__itog--right">
-								<a class="button-1 button-2" href="#">Оформить заказ</a>
-				  		</div>
+						<div class="nam nam-txt">
+							<h4 class="item__info">TOSHIBA HDTS425EZSTA</h4>
+							<div class="name12">Оперативная память </div>
+							<div class="name2">Белый / XL 48 /480 ГБ / 2.5" SATA III</div>
+							<div class="name2">+ 2 подарка (дополнительная щетка, набор валиков)</div>
 						</div>	
+						<div class="nam nam-quan">
+							<div class="df-number">
+								<span class="dfbutton dfbutton2">-</span>
+								<input type="text" value="1" disabled="disabled">  
+								<span class="dfbutton dfbutton3">+</span>
+							</div>	
+						</div>
+						<div class="nam nam-price">
+							<div class="item__price">
+								<strong>52 380 Р</strong>
+								<div class="name12"> 1шт.: 26 190 р.</div>	
+							</div>
+						</div>
+						<button class="nam-close__btn"></button>
+					</div>
+				</div>
+				<div class="box-2">
+					<div class="clearfix">
+						<div class="nam nam-img">
+							<div class="item__img">
+								<img src="img/busket_v3.jpg" alt="">
+							</div>
+						</div>
+						<div class="nam nam-txt">
+							<h4 class="item__info">Игровой компьютер REVOLT X</h4>
+							<div class="name12">Игровые компьютеры</div>
+							<div class="name2">Core i7 6700k / 2xGeForce GTX 1080 / 32Gb DDR4 / SSD 250 Gb / HDD 2Tb</div>
+							<div class="name2">+ Специальное предложение X3Экономия 231Р.</div>
+						</div>	
+						<div class="nam nam-quan">
+							<div class="df-number">
+								<span class="dfbutton dfbutton2">-</span>
+								<input type="text" value="1" disabled="disabled">  
+								<span class="dfbutton dfbutton3">+</span>
+							</div>	
+						</div>
+						<div class="nam nam-price">
+							<div class="item__price">
+								<strong>278 440 Р</strong>
+								<div class="name12"> 1шт.: 278 440 р.</div>	
+							</div>
+						</div>
+						<button class="nam-close__btn"></button>
+					</div>
+				</div>
+				<div class="box-2">
+					<div class="clearfix">
+						<div class="nam nam-img">
+							<div class="item__img">
+								<img src="img/busket_v4.jpg" alt="">
+							</div>
+						</div>
+						<div class="nam nam-txt">
+							<h4 class="item__info">умная метка nut</h4>
+							<div class="name12">Аксессуары к мобильным устройствам</div>
+							<div class="name2">Батарея СК2032 / Макс. дальн. 50 метров / Iphone 4S и выше / OC Android 4.3 и выше</div>
+							<div class="name2">+ Специальное предложение X10Экономия 231Р и подарок (Мягкий магнитный калькулятор на холодильник)</div>
+						</div>	
+						<div class="nam nam-quan">
+							<div class="df-number">
+								<span class="dfbutton dfbutton2">-</span>
+								<input type="text" value="1" disabled="disabled">  
+								<span class="dfbutton dfbutton3">+</span>
+							</div>	
+						</div>
+						<div class="nam nam-price">
+							<div class="item__price">
+								<strong>9 979 Р</strong>
+								<div class="name12"> 1шт.: 1 070р.</div>	
+							</div>
+						</div>
+						<button class="nam-close__btn"></button>
+					</div>
+				</div>
+				<div class="box-2">
+					<div class="clearfix">
+						<div class="nam nam-img">
+							<div class="item__img">
+								<img src="img/busket_v5.jpg" alt="">
+							</div>
+						</div>
+						<div class="nam nam-txt">
+							<h4 class="item__info">Эксклюзивная модель танка Пантера</h4>
+							<div class="name12">Игровые компьютеры</div>
+							<div class="name2">Ручная работа / Проработка / Масштаб 1к35 / Надежная упаковка</div>
+							<div class="name2"></div>
+						</div>	
+						<div class="nam nam-quan">
+							<div class="df-number">
+								<span class="dfbutton dfbutton2">-</span>
+								<input type="text" value="1" disabled="disabled">  
+								<span class="dfbutton dfbutton3">+</span>
+							</div>	
+						</div>
+						<div class="nam nam-price">
+							<div class="item__price">
+								<strong>8 200 Р</strong>
+								<div class="name12"> 1шт.: 8 200 р.</div>	
+							</div>
+						</div>
+						<button class="nam-close__btn"></button>
+					</div>
+				</div>
+				<div class="box-itog">
+					<div class="clearfix">
+						<div class="item__itog">
+							<div class="itog">Итого: 4 товара на сумму <span> 350 980 Р.</span> 
+							</div>
+						</div>
+						<div class="item__itog item__itog--right">
+							<a class="button-1 button-2" href="#">Оформить заказ</a>
+			  		</div>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="section byu">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h3>Вместе с этим товаром <span>покупают:</span></h3>
+				<p>Дополните ваш заказ и не переплачивайте за доставку</p>
+				<div class="items-wrap clearfix">
+					<div class="df-catalog__product df-catalog__product_view col-md-3">
+						<div class="df-catalog__img df-catalog__img_view">
+							<img src="img/dftovar5.png" alt="" class="df-imgproduct">
+							<div class="df-miniimage">
+								<div class="df-catalog__ico df-catalog__ico_view">
+									<span class="df-catalog__show df-catalog__show_view">сделан видеоразбор</span>
+									<div class="df-catalog_image df-catalog_image_view">
+										<img src="img/dfi2.svg" alt="">
+									</div>
+								</div>
+								<div class="df-catalog__ico df-catalog__ico_view">
+									<span class="df-catalog__show df-catalog__show_view">хит октября</span>
+									<div class="df-catalog_image df-catalog_image_view">
+										<img src="img/dfi4.svg" alt="">
+									</div>
+								</div>				
+							</div>
+						</div>
+						<div class="df-catalog-txtbox df-catalog-txtbox_view">
+							<div class="df-alltxt">
+								<h4>Модуль памяти <br>HyperX Savage DDR4 (16gb)</h4>
+								<span class="df-nameproduct">Оперативная память</span>
+								<p>Скорость до 3000 МГц и низкая латентность обеспечивают..</p>
+							</div>
+							<div class="dfboxprice">			
+								<div class="df-catalog__busket df-catalog__busket_view">					
+									<span class="df-catalog__newprice2 df-catalog__newprice_view2">8 990 р</span>
+								</div>
+								<a href="#" class="button-1 button-1_view">в корзину</a>
+							</div>
+						</div>
+					</div>
+					<div class="df-catalog__product df-catalog__product_view col-md-3">
+						<div class="df-catalog__img df-catalog__img_view">
+							<img src="img/dftovar6.png" alt="" class="df-imgproduct">
+							<div class="df-miniimage">
+								<div class="df-catalog__ico df-catalog__ico_view">
+									<span class="df-catalog__show df-catalog__show_view">+ подарок</span>
+									<div class="df-catalog_image df-catalog_image_view">
+										<img src="img/dfi1.svg" alt="">
+									</div>
+								</div>					
+							</div>
+						</div>
+						<div class="df-catalog-txtbox df-catalog-txtbox_view">
+							<div class="df-alltxt">
+								<h4>TH-900 Black <br> Limited Edition</h4>
+								<span class="df-nameproduct">Наушники</span>	
+							</div>
+							<div class="dfboxprice">			
+								<div class="df-catalog__busket df-catalog__busket_view">					
+									<span class="df-catalog__newprice2 df-catalog__newprice_view2">13 590 р</span>
+								</div>
+								<a href="#" class="button-1 button-1_view">в корзину</a>
+							</div>
+						</div>
+					</div>
+					<div class="df-catalog__product df-catalog__product_view col-md-3">
+						<div class="df-catalog__img df-catalog__img_view">
+							<img src="img/dftovar7.png" alt="" class="df-imgproduct">		
+						</div>
+						<div class="df-catalog-txtbox df-catalog-txtbox_view">
+							<div class="df-alltxt">
+								<h4>Игровой <br>компьютер <br>REVOLT X</h4>
+								<span class="df-nameproduct">Игровые компьютеры</span>	
+							</div>
+							<div class="dfboxprice">			
+								<div class="df-catalog__busket df-catalog__busket_view df-block">
+									от 400 000 р
+								</div>
+								<a href="#" class="button-1 button-1_view">в корзину</a>
+							</div>
+						</div>
+					</div>
+					<div class="df-catalog__product df-catalog__product_view col-md-3">
+						<div class="df-catalog__img df-catalog__img_view">
+							<img src="img/dftovar8.png" alt="" class="df-imgproduct">
+							<div class="df-miniimage">					
+								<div class="df-catalog__ico df-catalog__ico_view">
+									<span class="df-catalog__show df-catalog__show_view">сделан видеоразбор</span>
+									<div class="df-catalog_image df-catalog_image_view">
+										<img src="img/dfi2.svg" alt="">
+									</div>
+								</div>					
+							</div>
+						</div>
+						<div class="df-catalog-txtbox df-catalog-txtbox_view">
+							<div class="df-alltxt df-alltxt3">
+								<h4>Fostex HP-A4BL</h4>
+								<span class="df-nameproduct">Усилители</span>
+								<p>это цифро-аналоговый преобразователь и усилитель для наушников, который представляет собой усовершенствованную версию модели HP-A4. Главное отличие новинки: наличие..</p>
+							</div>
+							<div class="dfboxprice">
+								<div class="df-catalog__busket df-catalog__busket_view">
+									<span class="df-catalog__oldprice df-catalog__oldprice_view">35 990 р</span>
+									<span class="df-catalog__newprice df-catalog__newprice_view">26 690 р</span>
+								</div>
+								<a href="#" class="button-1 button-1_view">в корзину</a>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
